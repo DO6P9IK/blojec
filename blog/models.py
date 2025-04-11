fdaa35cb14bc828f8ada00c6849f78432d8589c5
DO6P9IK <stavkrayinvest@mail.ru>
2025-04-11 19:30:35 +0300

    Добавил резольвер URL

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 
@@ -39,3 +40,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.id])
+    
