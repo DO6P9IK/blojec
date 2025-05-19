68b700ca22fbf06511856ea06089343c8434b48b
DO6P9IK <stavkrayinvest@mail.ru>
2025-05-19 17:30:32 +0300

    добавил сласс обработки формы комментариев

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Comment
 
 
 class EmailPostForm(forms.Form):
@@ -7,4 +8,8 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField(label="Кому")
     comments = forms.CharField(required=False, widget=forms.Textarea, label="Комментарий")   
 
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
     
