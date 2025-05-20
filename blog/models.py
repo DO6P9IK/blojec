88a2dab4fe1ad736036d162cf86ac8fefd3fc37f
DO6P9IK <stavkrayinvest@mail.ru>
2025-05-20 17:06:39 +0300

    добавил систему тэгирования

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -28,9 +29,12 @@ class Post(models.Model):
     status = models.CharField(
         max_length=2, choices=Status.choices, default=Status.DRAFT
     )
+    
 
     objects = models.Manager()  # менеджер, применяемый по умолчанию
     published = PublishedManager()  # пользовательский менеджер
+    tags = TaggableManager() # менеджер тэгов
+
 
     class Meta:
         ordering = ["-publish"]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import ListView
+from taggit.models import Tag
 from django.core.mail import send_mail
 from django.conf import settings
 from blojec import settings
@@ -11,35 +11,39 @@ from django.views.decorators.http import require_POST
 
 def post_share(request, post_id):
     # Извлечь пост по его идентификатору id
-    post = get_object_or_404(Post,
-                             id=post_id,
-                             status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     sent = False
 
-    if request.method == 'POST':
+    if request.method == "POST":
         # Форма была передана на обработку
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Поля формы успешно прошли валидацию
             cd = form.cleaned_data
-            post_url = request.build_absolute_uri(
-                post.get_absolute_url())
-            subject = f"{cd['name']} рекомендует Вам прочитать " \
-                      f"{post.title}"
-            message = f"Прочтите {post.title} по адресу {post_url}\n\n" \
-                      f"{cd['name']} ({cd['email']}) комментирует: {cd['comments']}"
-            send_mail(subject, message, settings.EMAIL_HOST_USER,
-                      [cd['to']])
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} рекомендует Вам прочитать " f"{post.title}"
+            message = (
+                f"Прочтите {post.title} по адресу {post_url}\n\n"
+                f"{cd['name']} ({cd['email']}) комментирует: {cd['comments']}"
+            )
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [cd["to"]])
             sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post,
-                                                    'form': form,
-                                                    'sent': sent})
+    return render(
+        request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
+    )
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags=tag)
+
+    # Постраничная разбивка по 3 на страницу
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get("page", 1)
     try:
@@ -52,31 +56,32 @@ def post_list(request):
         # Если page_number находится вне диапазона, то
         # выдать последнюю страницу результатов
         posts = paginator.page(paginator.num_pages)
-    return render(request, "blog/post/list.html", {"posts": posts})
+    return render(request, "blog/post/list.html", {"posts": posts, "tag": tag})
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post,
-                             status=Post.Status.PUBLISHED,
-                             slug=post,
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
     # Список активных комментариев к этому посту
     comments = post.comments.filter(active=True)
     # Форма для комментирования пользователями
     form = CommentForm()
-    return render(request,
-                  'blog/post/detail.html',
-                  {'post': post,
-                   'comments': comments,
-                   'form': form})
+    return render(
+        request,
+        "blog/post/detail.html",
+        {"post": post, "comments": comments, "form": form},
+    )
+
 
 @require_POST
 def post_comment(request, post_id):
-    post = get_object_or_404(Post,
-                             id=post_id,
-                             status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     comment = None
     # Комментарий был отправлен
     form = CommentForm(data=request.POST)
@@ -87,7 +92,8 @@ def post_comment(request, post_id):
         comment.post = post
         # Сохранить комментарий в базе данных
         comment.save()
-    return render(request, 'blog/post/comment.html',
-                  {'post': post,
-                   'form': form,
-                   'comment': comment})
+    return render(
+        request,
+        "blog/post/comment.html",
+        {"post": post, "form": form, "comment": comment},
+    )
