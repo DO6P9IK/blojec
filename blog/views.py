cfff32ec60f4960e82725a6377fefbebb2a8f8a6
DO6P9IK <stavkrayinvest@mail.ru>
2025-05-20 17:19:28 +0300

    добавил фичу похожих постов

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 from taggit.models import Tag
 from django.core.mail import send_mail
 from django.conf import settings
@@ -60,23 +61,31 @@ def post_list(request, tag_slug=None):
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(
-        Post,
-        status=Post.Status.PUBLISHED,
-        slug=post,
-        publish__year=year,
-        publish__month=month,
-        publish__day=day,
-    )
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
     # Список активных комментариев к этому посту
     comments = post.comments.filter(active=True)
-    # Форма для комментирования пользователями
+
+    # Форма для комментариев пользователей
     form = CommentForm()
-    return render(
-        request,
-        "blog/post/detail.html",
-        {"post": post, "comments": comments, "form": form},
-    )
+
+    # Список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+                        .order_by('-same_tags', '-publish')[:4]
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 
 @require_POST
