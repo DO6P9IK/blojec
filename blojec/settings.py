42baddcb3026e6fdf21990b27dc756757ad7f8be
DO6P9IK <stavkrayinvest@mail.ru>
2025-05-23 21:47:10 +0300

    перенес данные в бд posgresql

--- blojec/settings.py
+++ blojec/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'blojec.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': '123123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
