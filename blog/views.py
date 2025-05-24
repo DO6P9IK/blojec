19da5486a8aee024c98798a15d4ad00d0014e1b0
DO6P9IK <stavkrayinvest@mail.ru>
2025-05-24 18:55:59 +0300

    Добавил выделение основ слов и ранжирование результатов

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,7 @@ from blojec import settings
 from .models import Post
 from .forms import EmailPostForm, CommentForm, SearchForm
 from django.views.decorators.http import require_POST
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 def post_share(request, post_id):
@@ -119,7 +119,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title', 'body')).filter(search=query)
+            search_vector = SearchVector('title', 'body', config='russian')
+            search_query = SearchQuery(query, config='russian')
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
     
     return render(request, 'blog/post/search.html', 
                   {'form': form, 'query': query, 'results': results})
