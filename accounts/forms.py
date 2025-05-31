29246902345d914e69b78b5e21572f18f3e0f6be
DO6P9IK <stavkrayinvest@mail.ru>
2025-05-31 23:11:15 +0300

    Отбутстрапил проект

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,38 +1,53 @@
+from re import L
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Profile
 
 
+
+
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100, label='Имя')
-    last_name = forms.CharField(max_length=100, label='Фамилия')
-    username = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    password1 = forms.CharField(max_length=50, widget=forms.PasswordInput(), label='Пароль')
-    password2 = forms.CharField(max_length=50, widget=forms.PasswordInput(), label='Повторите пароль')
+    first_name = forms.CharField(max_length=100, label='Имя', widget=forms.TextInput(
+        attrs={"class": "form-control mb-1", 'placeholder': 'Введите Имя'}))
+    last_name = forms.CharField(max_length=100, label='Фамилия', widget=forms.TextInput(
+        attrs={"class": "form-control mb-1", 'placeholder': 'Введите Фамилию'}))
+    username = forms.CharField(max_length=100, label='Логин', widget=forms.TextInput(
+        attrs={"class": "form-control mb-1", 'placeholder': 'Введите Логин'}))
+    email = forms.EmailField(
+        widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Введите Email'}))
+    password1 = forms.CharField(max_length=50, label='Пароль', widget=forms.PasswordInput(
+        attrs={"class": "form-control mb-1", 'placeholder': 'Введите Пароль'}))
+    password2 = forms.CharField(max_length=50, label='Повторите пароль', widget=forms.PasswordInput(
+        attrs={"class": "form-control mb-1", 'placeholder': 'Подтвердите Пароль'}))
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
 
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(max_length=100,
-                               required=True,
-                               widget=forms.TextInput(attrs={'placeholder': 'Имя'}), label='Имя')
+                               required=True, label='Логин',
+                               widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Ваш логин'}))
     password = forms.CharField(max_length=50,
-                               required=True,
-                               widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}), label='Пароль')
+                               required=True, label='Пароль',
+                               widget=forms.PasswordInput(
+                                   attrs={"class": "form-control mb-1", 'placeholder': 'Введите пароль'}))
     remember_me = forms.BooleanField(required=False, label='Запомнить меня')
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'remember_me']
 
 
 class UpdateUserForm(forms.ModelForm):
     username = forms.CharField(max_length=100,
-                               required=True,
-                               widget=forms.TextInput())
-    email = forms.EmailField(required=True,
-                             widget=forms.TextInput())
+                               required=True, label='Логин',
+                               widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Ваш логин'}))
+    email = forms.EmailField(required=True, label='Email',
+                             widget=forms.TextInput(attrs={"class": "form-control mb-1", 'placeholder': 'Ваш Email'}))
 
     class Meta:
         model = User
@@ -40,8 +55,8 @@ class UpdateUserForm(forms.ModelForm):
 
 
 class UpdateProfileForm(forms.ModelForm):
-    avatar = forms.ImageField(widget=forms.FileInput())
-    bio = forms.CharField(widget=forms.Textarea(attrs={'rows': 5}))
+    avatar = forms.ImageField(label='Аватар', widget=forms.FileInput(attrs={"class": "form-control mb-1"}))
+    bio = forms.CharField(label='О себе', widget=forms.Textarea(attrs={"class": "form-control"}))
 
     class Meta:
         model = Profile
--- blog/forms.py
+++ blog/forms.py
@@ -3,17 +3,69 @@ from .models import Comment
 
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25, label="Имя")
-    email = forms.EmailField(label="Email")
-    to = forms.EmailField(label="Кому")
-    comments = forms.CharField(required=False, widget=forms.Textarea, label="Комментарий")   
+    name = forms.CharField(
+        max_length=25,
+        required=True,
+        label="Имя",
+        widget=forms.TextInput(
+            attrs={"class": "form-control mb-1", "placeholder": "Ваше Имя"}
+        ),
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.TextInput(
+            attrs={"class": "form-control mb-1", "placeholder": "E-Mail"}
+        ),
+    )
+    to = forms.EmailField(
+        required=True,
+        label="Кому",
+        widget=forms.TextInput(
+            attrs={"class": "form-control mb-1", "placeholder": "Кому отправить"}
+        ),
+    )
+    comments = forms.CharField(
+        required=False,
+        label="Комментарий",
+        widget=forms.Textarea(
+            attrs={"class": "form-control mb-1", "placeholder": "Ваш комментарий"}
+        ),
+    )
+
 
 class CommentForm(forms.ModelForm):
+    name = forms.CharField(
+        required=True,
+        label="Имя",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Ваше Имя"}
+        ),
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(
+            attrs={"class": "form-control", "placeholder": "Email"}
+        ),
+    )
+    body = forms.CharField(
+        required=True,
+        label="Текст",
+        widget=forms.Textarea(
+            attrs={"class": "form-control", "placeholder": "Ваш комментарий"}
+        ),
+    )
+
     class Meta:
         model = Comment
-        fields = ['name', 'email', 'body']
+        fields = ["name", "email", "body"]
 
 
 class SearchForm(forms.Form):
-    query = forms.CharField(label='Запрос')
-
+    query = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control mb-1",
+                "placeholder": "Введите форазу для поиска...",
+            }
+        )
+    )
